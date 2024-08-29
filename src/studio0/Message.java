3aca7f5c2f2bff81e1bb243f825b8531eee15bec
jeffreyliu <jeffreyliu@localhost>
2024-08-29 10:56:23 -0500

    Merge branch 'master' of https://github.com/CSE131FL24/studio-0-liu-kim-robert-studio0.git

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Hi Allen, Aiden, Robert!");
+		System.out.println("Hi Allen, Aiden, Robert! This is Jeffrey!");
 	}
 }
